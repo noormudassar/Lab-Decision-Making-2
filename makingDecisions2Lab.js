5160a829d7a478b09482211cb6c93003317b70fc
Noor Mudassar <noormudassar97@gmail.com>
2023-06-23 16:48:19 -0400

    compleated problems 1-3

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -7,6 +7,7 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+let firstItem = arr[0];
 
 
 ////////// PROBLEM 2 //////////
@@ -15,6 +16,9 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+let lastItemRemoved = arr.pop();
+// console.log(arr)
+// console.log(lastItemRemoved)
 
 ////////// PROBLEM 3 //////////
 
@@ -27,6 +31,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
+for (let i = 0; i < family.length; i++) {
+  console.log(family[i])
+}
 
 
 
